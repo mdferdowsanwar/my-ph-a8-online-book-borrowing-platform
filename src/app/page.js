c72d957da97ade3eda16ac46ccd946cc01da0d39
mdferdowsanwar <ferdows.cse@gmail.com>
2026-04-30 19:32:57 +0600

    Added a json data

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,10 @@ export default function Home() {
     <main className="w-9/12 mx-auto space-y-4">
       <Banner></Banner>
       <NewArrival></NewArrival>
-      <h2>Homepage</h2>
+      <div className="grid grid-cols-12">
+        <div className="col-span-4 border">All Categories</div>
+        <div className="col-span-8 border">Main Content</div>
+      </div>
     </main>
   );
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='bg-[#ffda79] py-20 rounded-lg'>
+        <div className='bg-[#ffda79] py-16 rounded-lg'>
             <div className='flex flex-col justify-center items-center gap-4'>
                 <h1 className='text-2xl sm:text-5xl font-bold'>Find Your Next Read</h1>
                 <button className='btn btn-neutral'>Browse Now</button>
